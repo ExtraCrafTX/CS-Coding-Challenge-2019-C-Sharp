2f0307ca33440c73837ddd4351003de21f303575
CS-CodingChallenge-UK <55763763+CS-CodingChallenge-UK@users.noreply.github.com>
2019-10-02 22:11:16 +0100

    Answers/Question1.cs

---
+++ Answers/Question1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Challenge_Skeleton.Answers
+{
+    public class Question1
+    {
+        public static int Answer(double initialLevelOfDebt, double interestPercentage, double repaymentPercentage)
+        {
+            //TODO: Please work out the solution;
+            return -1;
+        }
+    }
+}
