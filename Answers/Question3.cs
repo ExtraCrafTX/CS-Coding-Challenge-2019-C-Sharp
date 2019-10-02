c5e224f398a1ae8bf27dac5486d16ec2c7fb650a
CS-CodingChallenge-UK <55763763+CS-CodingChallenge-UK@users.noreply.github.com>
2019-10-02 22:11:18 +0100

    Answers/Question3.cs

---
+++ Answers/Question3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C_Sharp_Challenge_Skeleton.Answers
+{
+    public class Question3
+    {
+        public static int Answer(int[] scores, int[] alice)
+        {
+            //TODO: Please work out the solution;
+            return -1;
+        }
+    }
+}
