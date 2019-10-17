25807eb8cad4833ed4f3318e01ff43c05d511dcf
Kaamil Jasani <kaamilj@yahoo.com>
2019-10-17 19:51:58 +0100

    Q2 sol

--- C_Sharp_Skeleton_2019/Answers/Question2.cs
+++ C_Sharp_Skeleton_2019/Answers/Question2.cs
@@ -1,10 +1,25 @@
-﻿namespace C_Sharp_Challenge_Skeleton.Answers
+﻿using System;
+
+namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question2
     {
         public static int Answer(int[] risk, int[] bonus, int[] trader)
         {
-            return 90;
+            int total = 0;
+            foreach(int skill in trader)
+            {
+                int best = 0;
+                for (int i = 0; i < risk.Length; i++)
+                {
+                    if (best < bonus[i] && risk[i] <= skill)
+                    {
+                        best = bonus[i];
+                    }
+                }
+                total += best;
+            }
+            return total;
         }
     }
 }
