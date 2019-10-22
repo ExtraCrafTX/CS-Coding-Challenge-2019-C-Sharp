3daedd03820f20c2d9e737e47b5eece3ef8228f0
Kaamil Jasani <kaamilj@yahoo.com>
2019-10-22 16:37:12 +0100

    Q6 improv maybe

--- C_Sharp_Skeleton_2019/Answers/Question6.cs
+++ C_Sharp_Skeleton_2019/Answers/Question6.cs
@@ -10,12 +10,11 @@ namespace C_Sharp_Challenge_Skeleton.Answers
             char end = input[0][input[0].Length - 1];
             for (int i = input.Length - 1; i >= 0; i--)
             {
-                string testing = input[i];
-                if (testing[0] != start || testing[testing.Length - 1] != end)
+                if (input[i][0] != start || input[i][input[i].Length - 1] != end)
                     continue;
                 return i;
             }
-            return 0;
+            return -1;
         }
     }
 }
