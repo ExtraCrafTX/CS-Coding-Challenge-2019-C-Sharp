1ecb7a251107c7eedd5c3385c68b14cafab5350c
Kaamil Jasani <kaamilj@yahoo.com>
2019-10-22 04:08:02 +0100

    Q3 improv

--- C_Sharp_Skeleton_2019/Answers/Question3.cs
+++ C_Sharp_Skeleton_2019/Answers/Question3.cs
@@ -7,77 +7,135 @@ namespace C_Sharp_Challenge_Skeleton.Answers
     {
         public static int Answer(int[] scores, int[] alice)
         {
-            SortedSet<int> set = new SortedSet<int>(scores);
-            set.CopyTo(scores);
-            int n = set.Count;
+            TreeNode root = new TreeNode(scores[scores.Length - 1]);
+            BuildTree(root, scores);
             Array.Sort(alice);
-            int lastS = n-1;
+            TreeNode current = getStart(root);
+            return getModePos(current, alice);
+        }
+
+        public static int getModePos(TreeNode current, int[] alice)
+        {
+            int currentPos = 1;
+            int lastPos = 0;
             int count = 0;
             int maxCount = 0;
             int maxVal = 0;
-            for(int i = alice.Length-1; i >= 0; i--)
+            for (int i = alice.Length - 1; i >= 0; i--)
             {
-                int s;
-                for(s = lastS; s>=0 && scores[s] > alice[i]; s--){}
-                if (s != lastS)
+                int score = alice[i];
+                while (current != null && score < current.val)
                 {
+                    if (current.left != null)
+                    {
+                        current = current.left;
+                        while (current.right != null)
+                            current = current.right;
+                    }
+                    else
+                    {
+                        while (current != null && !current.big)
+                        {
+                            current = current.parent;
+                        }
+                        if(current != null)
+                            current = current.parent;
+                    }
+                    currentPos++;
+                }
+                if (currentPos != lastPos)
+                {
+                    lastPos = currentPos;
                     count = 1;
-                    lastS = s;
                 }
                 else
                 {
                     count++;
                 }
-                if(count >= maxCount)
+                if (count >= maxCount)
                 {
                     maxCount = count;
-                    maxVal = s;
+                    maxVal = currentPos;
                 }
             }
-            return n - maxVal;
+            return maxVal;
         }
 
-        public static int findPos(int[] leaderboard, int score)
+        public static TreeNode getFinish(TreeNode node)
         {
-            int l = 0;
-            int r = leaderboard.Length - 1;
-            while (l < r)
-            {
-                int m = (l + r) / 2;
-
-                if (score == leaderboard[m])
-                {
-                    return m;
-                }
-                else if (score > leaderboard[m])
-                {
-                    l = m + 1;
-                }
-                else
-                {
-                    r = m - 1;
-                }
-            }
-            if (l >= r)
-            {
-                if (score >= leaderboard[l])
-                {
-                    return l;
-                }
-                else
-                {
-                    return l - 1;
-                }
-            }
-            return 0;
+            while (node.left != null)
+                node = node.left;
+            return node;
         }
 
-        private class ReverseComparer : IComparer<int>
+        public static TreeNode getStart(TreeNode node)
         {
-            public int Compare(int a, int b)
+            while (node.right != null)
+                node = node.right;
+            return node;
+        }
+
+        public static void BuildTree(TreeNode root, int[] scores)
+        {
+            for (int i = scores.Length - 2; i >= 0; i--)
             {
-                return b - a;
+                TreeNode curr = root;
+                int score = scores[i];
+                while (true)
+                {
+                    if (score == curr.val)
+                        break;
+                    else if (score < curr.val)
+                    {
+                        if (curr.left == null)
+                        {
+                            curr.left = new TreeNode(score, curr);
+                        }
+                        else
+                        {
+                            curr = curr.left;
+                        }
+                    }
+                    else
+                    {
+                        if (curr.right == null)
+                        {
+                            curr.right = new TreeNode(score, curr, true);
+                        }
+                        else
+                        {
+                            curr = curr.right;
+                        }
+                    }
+                }
             }
+        }
+    }
+
+    public class TreeNode
+    {
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode parent;
+        public bool big;
+        public int val;
+
+        public TreeNode(int val)
+        {
+            this.val = val;
+        }
+
+        public TreeNode(int val, TreeNode parent)
+        {
+            this.val = val;
+            this.parent = parent;
+        }
+
+        public TreeNode(int val, TreeNode parent, bool big)
+        {
+            this.val = val;
+            this.parent = parent;
+            this.big = big;
         }
     }
 }
