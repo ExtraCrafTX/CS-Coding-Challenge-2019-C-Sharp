7585cd41ca0e6985111f1c9d06ec5060340dc195
Kaamil Jasani <kaamiljasani@gmail.com>
2019-10-23 07:21:29 +0100

    Q5 improv probably

--- C_Sharp_Skeleton_2019/Answers/Question5.cs
+++ C_Sharp_Skeleton_2019/Answers/Question5.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
+
 namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question5
     {
         public static int Answer(int[] input)
         {
-            if (input[0] > 30)
-                return input[0] / 6;
-            else if (input[0] > 18)
-                return input[0] / 4;
-            else
-                return input[0] / 3 + 1;
+            return (int)(1.3 * Math.Sqrt(input[0]));
         }
     }
 }
