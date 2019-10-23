9d4ef3c62e9cdb3500c5af70895ad4fc4ac65f07
Kaamil Jasani <kaamilj@yahoo.com>
2019-10-23 08:25:34 +0100

    Q4 improv

--- C_Sharp_Skeleton_2019/Answers/Question4.cs
+++ C_Sharp_Skeleton_2019/Answers/Question4.cs
@@ -4,13 +4,13 @@ namespace C_Sharp_Challenge_Skeleton.Answers
 {
     public class Question4
     {
-        public static int Answer(int[] v, int[] c, int mc)
+        unsafe public static int Answer(int[] v, int[] c, int mc)
         {
             Array.Sort(v, c);
             int total = 0;
             for(int i = v.Length-1; i >= 0; i--)
             {
-                if (c[i] < mc)
+                if (c[i] <= mc)
                 {
                     total += v[i];
                     mc -= c[i];
